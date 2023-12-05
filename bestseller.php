4e6639a3821015f7c5bca3986fdba649c0453601
BhavyaSuseelan28 <149084001+BhavyaSuseelan28@users.noreply.github.com>
2023-12-05 20:16:41 +0530

    Create bestseller.php
    best sellers

---
+++ bestseller.php
@@ -0,0 +1,89 @@
+ <html>
+ <head>
+ <title>best sellers</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css"> 
+   
+ </head>
+ <body>
+ 	 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+	
+ <?php include('view/template/header.php');?>
+ <div class="container text-center" >
+  <div class="row align-items-start">
+  
+   
+	  <div id="products">
+	  
+	  </div>
+
+ <div class="col-3">
+	  <div class="card" >
+  <img src="KISSAN.jpg" class="card-img-top" alt="...">
+  <div class="cardbody">
+    <h5 class="pro_name">KISSAN JAM</h5>
+    <p class="pro_price">45</p>
+    <a href="addtocart" class="btn btn-outline-danger">ADD TO CART</a>
+  </div>
+</div>
+	  </div>
+	  
+	
+	  <div class="col-3">
+	  <div class="card" >
+  <img src="MAGGIE.JPEG" class="card-img-top" alt="...">
+  <div class="cardbody">
+    <h5 class="pro_name">MAGGIE</h5>
+    <p class="pro_price">45</p>
+    <a href="addtocart" class="btn btn-outline-danger">ADD TO CART</a>
+  </div>
+</div>
+	  </div>
+	  
+	  <div class="col-3">
+	  <div class="card" >
+  <img src="chocolatesilk.jpeg" class="card-img-top" alt="...">
+  <div class="cardbody">
+    <h5 class="pro_name">CHOCOLATE</h5>
+    <p class="pro_price">75</p>
+    <a href="addtocart" class="btn btn-outline-danger">ADD TO CART</a>
+  </div>
+</div>
+	  </div>
+	  
+	  <div class="col-3">
+	  <div class="card" >
+  <img src="darkchoco.png" class="card-img-top" alt="...">
+  <div class="cardbody">
+    <h5 class="pro_name">DARK CHOCO</h5>
+    <p class="pro_price">100 </p>
+    <a href="addtocart" class="btn btn-outline-danger">ADD TO CART</a>
+  </div>
+</div>
+</div>
+ <div class="col-3">
+	  <div class="card" >
+  <img src="icecream.jpeg" class="card-img-top" alt="ice cream">
+  <div class="cardbody">
+    <h5 class="pro_name">Ice cream</h5>
+    <p class="pro_price">150</p>
+    <a href="addtocart" class="btn btn-outline-danger">ADD TO CART</a>
+  </div>
+</div>
+	  </div> <div class="col-3">
+	  <div class="card" >
+  <img src="lays.jpeg" class="card-img-top" alt="lays">
+  <div class="cardbody">
+    <h5 class="pro_name">lays</h5>
+    <p class="pro_price">30</p>
+    <a href="addtocart" class="btn btn-outline-danger">ADD TO CART</a>
+  </div>
+</div>
+	  </div>
+	  </div>
+	  </div>
+	  </div>
+</body>	  
+</html>
