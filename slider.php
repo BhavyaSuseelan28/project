12f022db95de30336cc69dcd4e875d3e3580afaa
BhavyaSuseelan28 <149084001+BhavyaSuseelan28@users.noreply.github.com>
2023-12-05 20:18:28 +0530

    Create slider.php

---
+++ slider.php
@@ -0,0 +1,23 @@
+<div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false" style="width:1500px; height:600px">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="market.jpeg" class="d-block w-100" alt="market">
+    </div>
+    <div class="carousel-item">
+      <img src="market2.jpeg" class="d-block w-100" alt="market">
+    </div>
+    <div class="carousel-item">
+      <img src="lays2.jpeg" class="d-block w-100" alt="lays">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+<br>
+<br><br><br><br><br><br>
