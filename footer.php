6ad14a9ddd790f860c1c367b81dae361c18dfb43
BhavyaSuseelan28 <149084001+BhavyaSuseelan28@users.noreply.github.com>
2023-12-05 20:15:59 +0530

    Create footer.php
    footer

---
+++ footer.php
@@ -0,0 +1,39 @@
+<footer style ="background-color:#09C3E8;text-align: center;
+  padding: 2px;">
+<div class="container  text-center" >
+    <div class="row align-items-start">
+	<div class="col-1"></div>
+	<div class="col-3">
+	
+	<h5>
+  Contact us</h5>
+  <a href="bhappi2001@gmail.com">EMAIL</a><br>
+<a href="webpage.php">WEBSITE</a><br>
+ <a href="bhappi2001@gmail.com">RETAILERS</a>
+  
+	</div>
+	<div class="col-3">
+<h5>
+  Get to know us </h5>
+	
+   <a href="bhappi2001@gmail.com">ABOUT US</a><br>
+   <a href="bhappi2001@gmail.com">RETAILERS</a>
+  
+	</div>
+	<div class="col-3">
+	<h5>
+  Let us help you</h5>
+     <a href="bhappi2001@gmail.com">PRIVACY</a><br>
+	      <a href="bhappi2001@gmail.com">ACCOUNT</a><br>
+		       <a href="bhappi2001@gmail.com">SETTINGS</a><br>
+			        <a href="bhappi2001@gmail.com">CUSTOMER SERVICE</a><br>
+
+
+
+
+
+	</div>
+	</div>
+</div>
+ 
+</footer>
