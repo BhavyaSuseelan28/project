c6a22c03eb3b900dd0849649fb26cc2619394806
BhavyaSuseelan28 <149084001+BhavyaSuseelan28@users.noreply.github.com>
2023-12-05 20:14:43 +0530

    Create pro_cards.php
    product cards

---
+++ pro_cards.php
@@ -0,0 +1,118 @@
+ <div class="container text-center" >
+  <div class="row align-items-start">
+  
+   
+	  <div id="products">
+	  
+	  </div>
+
+ <div class="col-3">
+	  <div class="card" >
+  <img src="KISSAN.jpg" class="card-img-top" alt="...">
+  <div class="cardbody">
+    <h5 class="pro_name"><b>KISSAN JAM</b></h5>
+    <p class="pro_price"><b> &#8377 45</b></p>
+     <button type="button" class="btn btn-outline-danger position-relative">
+  ADD TO CART
+  <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+    25/-
+    <span class="visually-hidden">BUY PRODUCT</span>
+  </span>
+</button>
+  </div>
+</div>
+	  </div>
+	  
+	
+	  <div class="col-3">
+	  <div class="card" >
+  <img src="MAGGIE.JPEG" class="card-img-top" alt="...">
+  <div class="cardbody">
+    <h5 class="pro_name">MAGGIE</h5>
+    <p class="pro_price"> &#8377 45</p>
+<button type="button" class="btn btn-outline-danger" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="top" data-bs-content="Top popover">
+  ADD TO CART
+</button>  
+</div>
+</div>
+	  </div>
+	  
+	  <div class="col-3">
+	  <div class="card" >
+  <img src="chocolatesilk.jpeg" class="card-img-top" alt="...">
+  <div class="cardbody">
+    <h5 class="pro_name">CHOCOLATE</h5>
+    <p class="pro_price">&#8377 75</p>
+<button type="button" class="btn btn-outline-danger" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="top" data-bs-content="Top popover">
+  ADD TO CART
+</button>  </div>
+</div>
+	  </div>
+	  
+	  <div class="col-3">
+	  <div class="card" >
+  <img src="darkchoco.png" class="card-img-top" alt="...">
+  <div class="cardbody">
+    <h5 class="pro_name">DARK CHOCO</h5>
+    <p class="pro_price">&#8377 100 </p>
+<button type="button" class="btn btn-outline-danger" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="top" data-bs-content="Top popover">
+  ADD TO CART
+</button>  </div>
+</div>
+	  </div>
+	   <div class="col-3">
+	  <div class="card" >
+  <img src="icecream.jpeg" class="card-img-top" alt="ice cream">
+  <div class="cardbody">
+    <h5 class="pro_name">Ice cream</h5>
+    <p class="pro_price">&#8377 150</p>
+<button type="button" class="btn btn-outline-danger" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="top" data-bs-content="Top popover">
+  ADD TO CART
+</button>  </div>
+</div>
+	  </div> 
+	  <div class="col-3">
+	  <div class="card" >
+  <img src="lays.jpeg" class="card-img-top" alt="lays">
+  <div class="cardbody">
+    <h5 class="pro_name">lays</h5>
+    <p class="pro_price">&#8377 30</p>
+<button type="button" class="btn btn-outline-danger" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="top" data-bs-content="Top popover">
+  ADD TO CART
+</button>  </div>
+</div>
+</div>
+  <div class="col-3">
+	  <div class="card" >
+  <img src="pepsi.jpeg" class="card-img-top" alt="lays">
+  <div class="cardbody">
+    <h5 class="pro_name">Pepsi</h5>
+    <p class="pro_price">&#8377 30</p>
+<button type="button" class="btn btn-outline-danger" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="top" data-bs-content="Top popover">
+  ADD TO CART
+</button>  </div>
+</div>
+</div>
+  <div class="col-3">
+	  <div class="card" >
+  <img src="milk.jpeg" class="card-img-top" alt="milk">
+  <div class="cardbody">
+    <h5 class="pro_name">AMUL MILK</h5>
+    <p class="pro_price">&#8377 90</p>
+<button type="button" class="btn btn-outline-danger" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="top" data-bs-content="Top popover">
+  ADD TO CART
+</button>  </div>
+</div>
+</div>  <div class="col-3">
+	  <div class="card" >
+  <img src="tea.jpeg" class="card-img-top" alt="tea">
+  <div class="cardbody">
+    <h5 class="pro_name">tea</h5>
+    <p class="pro_price">&#8377 50</p>
+<button type="button" class="btn btn-outline-danger" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="top" data-bs-content="Top popover">
+  ADD TO CART
+</button>  </div>
+</div>
+</div>
+</div>
+</div>
