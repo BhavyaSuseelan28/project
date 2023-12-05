bc395bb11f61bff77b2d73c8fbc821055e768c15
BhavyaSuseelan28 <149084001+BhavyaSuseelan28@users.noreply.github.com>
2023-12-05 20:19:32 +0530

    Create customer_service.php

---
+++ customer_service.php
@@ -0,0 +1,32 @@
+ <html>
+ <head>
+ <title>customer service</title>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css"> 
+ </head>
+ <body>
+ <?php include('view/template/header.php');?>
+<div class="container" style="padding: 2px">
+	<div class="row align-items-start">
+	<div class="col-5" style="padding: 5px; border-style: outset">
+	<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Send us you feedback</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+</div>
+<div class="col-1"></div>
+<div class="col-5">
+<div class="mb-3" style="padding: 5px;">
+<h3><em>ABOUT US</em><h3>
+<h6><i>Our mission at is to empower businesses, entrepreneurs, and creative minds to showcase and sell their products to a discerning audience. We believe in the diversity and uniqueness of each product, and our platform is designed to celebrate this individuality.</i></h6>
+</div>
+</div>
+</div>
+</div>
+<?php include('view/template/footer.php');?>
+</body>
+</html>
