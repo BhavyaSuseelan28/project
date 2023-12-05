53591702b5ee3841b69468ae0fe9274e1b672c26
BhavyaSuseelan28 <149084001+BhavyaSuseelan28@users.noreply.github.com>
2023-12-05 20:15:27 +0530

    Create header.php
    header

---
+++ header.php
@@ -0,0 +1,69 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary" >
+  <div class="container-fluid" style ="background-image: linear-gradient(to right, #2FCAE9,white,#2FCAE9)">
+    <a class="navbar-brand" href="#"><B>FOODIE</B></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle d-inline-flex focus-ring py-1 px-2 text-decoration-none border rounded-2" style="--bs-focus-ring-color: rgba(var(--bs-success-rgb), .25)" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            All
+          </a>
+          <ul class="dropdown-menu">
+		  <p class="text-center"><b><em>TRENDING</em></b></p>
+            <li>		 <a class="dropdown-item" href="sweetstooth.php">Sweet tooth</a></li>
+		 
+            <li><a class="dropdown-item" href="#">Snacks</a></li>
+			    <li><hr class="dropdown-divider"></li>
+			<p class="text-center"><b><em>SHOP BY CATEGORY</em></b></p>
+            <li><a class="dropdown-item" href="#">Home essentials</a></li>
+            <li><a class="dropdown-item" href="#">Tea coffee</a></li>
+			<li><a class="dropdown-item" href="#">Rice</a></li
+			<li><a class="dropdown-item" href="#">Dairy</a></li>
+
+
+			     <li><hr class="dropdown-divider"></li>
+
+				<p class="text-center"><b><em>HELP AND SETTINGS</em></b></p>
+			<li><a class="dropdown-item" href="#">Buy again</a></li>
+			<li><a class="dropdown-item" href="#">Accounts</a></li>
+			<li><a class="dropdown-item" href="#">Address</a></li>
+			<li><a class="dropdown-item" href="#">Orders</a></li>
+			 <li><a class="dropdown-item" href="#">Setting</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="customer_service.php">Customer care</a></li>
+          </ul>
+        </li>
+	   <li class="nav-item">
+	 
+          <a class="nav-link active" aria-current="page" href="webpage.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a  href="bestseller.php" class="nav-link" >Best Sellers</a>
+        </li>
+		<li class="nav-item">
+          <a class="nav-link" href="todays_deal.php">TODAYS DEALS</a>
+        </li>
+		<li class="nav-item">
+          <a class="nav-link" href="#">Gifts</a>
+        </li>
+       
+       <li class="nav-item">
+          <a class="nav-link" href="#">Customer Service</a>
+        </li>
+		
+		<li><button type="button" class="btn btn-primary">
+  <em>YOUR CART</em><span class="badge text-bg-secondary"></span>
+</button></li>
+
+      </ul>
+	  
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+
+    </div>
+  </div>
+</nav>
